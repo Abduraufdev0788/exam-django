91d15bffef870bfc9cdd2006f99f028a751b20d3
abdurauf0788 <turkeynumber063@gmail.com>
2025-11-25 01:52:37 +0500

    put request

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Game
+from .views import Game, GameDetailView
 
 urlpatterns = [
-    path("games/", Game.as_view(), name="created_game")
+    path("games/", Game.as_view(), name="created_game"),
+    path("games/<int:game_id>/", GameDetailView.as_view(), name="game_detail"),
 ]
--- games/views.py
+++ games/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views import View
 import json
 
@@ -51,5 +52,53 @@ class Game(View):
             }
 
             return JsonResponse(context, status = 201)
+    
+class GameDetailView(View):
+    def put(self, request:HttpRequest, game_id:int)->JsonResponse:
+
+        game = get_object_or_404(Games, id = game_id)
+
+        try:
+            data = json.loads(request.body.decode())
+        except json.JSONDecodeError:
+            return JsonResponse({"message": "Invalid JSON"}, status=400)
+        
+        title = data.get("title")
+        location = data.get("location")
+        start_date = data.get("start_date")
+        description = data.get("description")
+        if title:
+            if len(title)>200:
+                return JsonResponse({"message": "title max 200 characters"}, status=400)
+            game.title = title
+        if location:
+            if len(location) > 100:
+                return JsonResponse({"message": "location max 100 characters"}, status=400)
+            game.location = location
+        
+        if start_date:
+            game.start_date = start_date
+        if description:
+            game.description = description
+
+        game.save()
+
+        context = {
+                "id": game.id,
+                "title": game.title,
+                "location": game.location,
+                "start_date": game.start_date,
+                "description": game.description,
+                "created_at": game.created_at
+            }
+        
+        return JsonResponse(context, status = 201)
+
+
+
+
+
+
+
 
 
